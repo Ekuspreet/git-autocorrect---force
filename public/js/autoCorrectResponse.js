8fc5236a72245073323bf6e47f6086e76cab254a
Raghavjit Rana <raghavjit002@gmail.com>
2024-08-16 17:24:37 +0530

    working partially

--- public/js/autoCorrectResponse.js
+++ public/js/autoCorrectResponse.js
@@ -1,6 +1,12 @@
 console.log("Auto Correct Response Script Loaded");
 
 function handleResponse(data){
-    console.log(data);
-    
+    console.log(data.matching[0]);
+
+    let text = textarea.value;
+
+    let updatedText = text.replace(/[^\s]?\w+\s$/, data.matching[0]+' ');
+
+    textarea.value = updatedText
+
 };
